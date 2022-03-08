31c753477e57f457b768531add9792e2f21be4b7
abbappii <bappi142434@gmail.com>
2022-03-08 21:12:21 +0600

    deploy environment

--- SearchEngine/settings.py
+++ SearchEngine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n(cp8i$3eu#a&)x)6apf2$)mt!8w$9^&d7!!ddyh1@c2z6z@ww
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
